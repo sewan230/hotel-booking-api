456ce34f02b6d631ad0542062924a759d016026d
sewan230 <sywanalbdallh@gmail.com>
2026-07-06 00:37:05 +0300

    exclude api routes from SPA catch-all

--- mywebsite/routes/web.php
+++ mywebsite/routes/web.php
@@ -2,12 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-
-
 Route::get('/{any}', function () {
     return view('home');
-})->where('any', '.*');
-// // this is for the forget_password
-// Route::get('/reset-password/{token}', function ($token) {
-//     return view('app');
-// })->name('password.reset');
+})->where('any', '^(?!api).*$');
